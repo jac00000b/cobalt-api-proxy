06d43a08a3bd36241c33b014e8d6def17ae42538
jac00000b <118233742+jac00000b@users.noreply.github.com>
2024-10-14 14:11:07 +0800

    add processing route

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import type { StatusCode } from "hono/utils/http-status";
 
 const app = new Hono();
 
@@ -40,6 +41,27 @@ async function getInstances() {
   );
 }
 
+function extractDomain(urlString: string): string {
+  const url = new URL(urlString);
+  const hostname = url.hostname;
+
+  const parts = hostname.split(".");
+
+  parts.pop();
+
+  if (parts.length > 2) {
+    parts.shift();
+  }
+
+  const domain = parts.join(".");
+
+  return domain;
+}
+
+function randomInstance(instances: Instance[]) {
+  return instances[Math.floor(Math.random() * instances.length)];
+}
+
 app.get("/", async (c) => {
   const instances = await getInstances();
   const latestInstance = instances.reduce((latest, instance) => {
@@ -67,6 +89,37 @@ app.get("/", async (c) => {
       commit: "unknown",
       remote: "jac00000b/cobalt-api-proxy",
     },
+  });
+});
+
+app.post("/", async (c) => {
+  const body = await c.req.json();
+  const instances = await getInstances();
+  let instance = randomInstance(instances);
+
+  try {
+    const service = extractDomain(body.url);
+    const suitableInstances = instances.filter(
+      (instance) => instance.services[service]
+    );
+
+    if (suitableInstances.length > 0)
+      instance = randomInstance(suitableInstances);
+  } catch (e) {
+    console.warn("Failed to find suitable instance, using random", e);
+  }
+
+  const res = await fetch(`${instance.protocol}://${instance.api}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+
+  return c.body(res.body, res.status as StatusCode, {
+    "Content-Type": "application/json",
+    "X-Instance": instance.api,
   });
 });
 
